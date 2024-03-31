93251e0e82af707da667bd12cf69bd31bd6a2d25
manhhung3004 <21522122@gm.uit.edu.vn>
2024-03-31 14:12:00 +0700

    done: compose for FE, BE and db

--- BE/routes/users.js
+++ BE/routes/users.js
@@ -4,25 +4,6 @@ const controller = require('../controller/Customer');
 const customer = require ('../model/Customer')
 const route = express.Router();
 
-// // POST login
-// route.post('/login', (req, res) => {
-//     const { email, password } = req.body; // Lấy email và password từ request body
-//     try {
-//       // Kiểm tra xem người dùng có tồn tại trong cơ sở dữ liệu hay không
-//       const user =  User.findOne({ where: { email: email, password: password },limit: 1 });
-//       if (user) {
-//         res.status(200).json({ message: 'Login successful!' });
-//         console.log("login successful");
-//       } else {
-//         res.status(401).json({ message: 'Invalid email or password' });
-//         console.log("login fail");
-//       }
-//     } catch (error) {
-//       next(error); // Xử lý lỗi nếu có
-//     }
-//   });
-
-
 // Parse JSON request body
 router.use(express.json());
 
